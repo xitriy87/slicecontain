8252a8e4673d5d6dec0cfc0b40c32bed13cdcc24
Alexeit <xitriy87@gmail.com>
2024-05-06 12:39:56 +0300

    Create slicecontain app

---
+++ slicecontain.go
@@ -0,0 +1,26 @@
+package slice-contain
+
+import (
+	"fmt"
+)
+
+func Contain(s []string,  x string) bool {
+	for _, v := range s {
+		if x == v {
+			return true
+		}
+	
+	}
+	return false
+}
+
+func ContainInt(s []int, i int) bool {
+	for _, v := range s {
+		if v == i {
+			return true
+		}
+	}
+	return false
+}
+
+
